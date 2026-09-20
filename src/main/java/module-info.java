e83b1d40b15b6ca4521f513b8cd7b46855a8391e
PahanSanajana <pahansanjana6226@gmail.com>
2026-09-20 11:18:01 +0530

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.mycompany.scrap.management.system {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.mycompany.scrap.management.system to javafx.fxml;
+    exports com.mycompany.scrap.management.system;
+}
